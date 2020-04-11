b0d902cdb2f45303882ac93832a7aa47634c7cf4
Arita Malik <hello@coderport.com>
2020-04-11 22:47:48 +0530

    initial commit

---
+++ src/main/java/com/codelovin/springboot/sonarclouddemo/SonarCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codelovin.springboot.sonarclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarCloudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelovin/springboot/sonarclouddemo/SonarCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelovin.springboot.sonarclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarCloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
