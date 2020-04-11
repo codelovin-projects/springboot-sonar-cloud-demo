f353e18509e35344d13cdcf1e52ec056710da72b
Arita Malik <hello@coderport.com>
2020-04-11 23:17:01 +0530

    Replaced System.err with Logger

--- src/main/java/com/codelovin/springboot/sonarclouddemo/SonarCloudDemoApplication.java
+++ src/main/java/com/codelovin/springboot/sonarclouddemo/SonarCloudDemoApplication.java
@@ -1,13 +1,17 @@
 package com.codelovin.springboot.sonarclouddemo;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SonarCloudDemoApplication {
 
+	private static Logger logger = Logger.getLogger(SonarCloudDemoApplication.class.getName());
+	
 	public static void main(String[] args) {
-		System.err.println("HARD_CODED_STRING");
+		logger.info("HARD_CODED_STRING");
 		SpringApplication.run(SonarCloudDemoApplication.class, args);
 	}
 
