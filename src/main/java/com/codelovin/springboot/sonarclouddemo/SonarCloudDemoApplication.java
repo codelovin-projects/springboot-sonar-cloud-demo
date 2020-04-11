b2754386724b5d894fe000a45afd8eff6d0590c6
Arita Malik <hello@coderport.com>
2020-04-11 23:11:31 +0530

    Replaced System.out with System.err

--- src/main/java/com/codelovin/springboot/sonarclouddemo/SonarCloudDemoApplication.java
+++ src/main/java/com/codelovin/springboot/sonarclouddemo/SonarCloudDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SonarCloudDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("HARD_CODED_STRING");
+		System.err.println("HARD_CODED_STRING");
 		SpringApplication.run(SonarCloudDemoApplication.class, args);
 	}
 
